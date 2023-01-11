830b0b3194749be802daf8c4308b30b831aa9b6e
SetiyoPramono <setiyopramono04@gmail.com>
2023-01-11 18:12:27 +0700

    ganti jugul

--- components/utils/sidebar.js
+++ components/utils/sidebar.js
@@ -5,9 +5,9 @@ export default function Sidebar() {
             {/* Sidebar - Brand */}
             <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div className="sidebar-brand-icon rotate-n-15">
-                    <i className="fas fa-laugh-wink" />
+                    {/* <i className="fas fa-laugh-wink" /> */}
                 </div>
-                <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div className="sidebar-brand-text mx-3">Pelayanan Service Mobil</div>
             </a>
             {/* Divider */}
             <hr className="sidebar-divider my-0" />
