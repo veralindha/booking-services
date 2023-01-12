4fec871e1bd96ac189d26fa90ee2e14f161a3abe
veralindha <fangkyistiawan@gmail.com>
2023-01-12 11:52:04 +0700

    change ext & fix error jquery

--- components/utils/scripts.js
+++ components/utils/scripts.js
@@ -2,13 +2,13 @@ import Script from 'next/script'
 export default function Scripts() {
     return (
         <>
-            <Script src="/dist/js/bootstrap.bundle.min.js" />
-            <Script src="/dist/js/datatables-demo.js" />
-            <Script src="/dist/js/dataTables.bootstrap.min.js" />
-            <Script src="/dist/js/jquery.dataTables.min.js" />
-            <Script src="/dist/js/jquery.easing.min.js" />
-            <Script src="/dist/js/jquery.min.js" />
-            <Script src="/dist/js/sb-admin-2.js" />
+            <script src="/dist/js/bootstrap.bundle.min.js" />
+            <script src="/dist/js/datatables-demo.js" />
+            <script src="/dist/js/dataTables.bootstrap.min.js" />
+            <script src="/dist/js/jquery.dataTables.min.js" />
+            <script src="/dist/js/jquery.easing.min.js" />
+            <script src="/dist/js/jquery.min.js" />
+            <script src="/dist/js/sb-admin-2.min.js" />
         </>
     )
 }
--- components/utils/sidebar.js
+++ components/utils/sidebar.js
@@ -1,35 +1,36 @@
 import Link from 'next/link'
 export default function Sidebar() {
-    return (
-        <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-            <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div className="sidebar-brand-icon rotate-n-15">
-                </div>
-                <div className="sidebar-brand-text mx-3">Pelayanan Service Mobil</div>
-            </a>
-            <hr className="sidebar-divider my-0" />
-            <li className="nav-item active">
-                <Link className="nav-link" href="/admin/dashboard">
-                    <i className="fas fa-fw fa-tachometer-alt" />
-                    <span>Dashboard</span>
-                </Link>
-            </li>
-            <hr className="sidebar-divider" />
-            <li className="nav-item">
-                <Link className="nav-link" href="/admin/tambahboking">
-                    <i className="fas fa-solid fa-list-ol" />
-                    <span>Tambah Boking</span></Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" href="/admin/history">
-                    <i className="fas fa-fw fa-table" />
-                    <span>History</span></Link>
-            </li>
-            <hr className="sidebar-divider" />
-            <div className="text-center d-none d-md-inline">
-                <button className="rounded-circle border-0" id="sidebarToggle" />
-            </div>
-        </ul>
+  return (
+    <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+      <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div className="sidebar-brand-icon rotate-n-15">
+        </div>
+        <div className="sidebar-brand-text mx-3">Pelayanan Service Mobil</div>
+      </a>
+      <hr className="sidebar-divider my-0" />
+      <li className="nav-item active">
+        <Link className="nav-link" href="/admin/dashboard">
+          <i className="fas fa-fw fa-tachometer-alt" />
+          <span>Dashboard</span>
+        </Link>
+      </li>
+      <hr className="sidebar-divider" />
+      <li className="nav-item">
+        <Link className="nav-link" href="/admin/tambahboking">
+          <i className="fas fa-solid fa-list-ol" />
+          <span>Tambah Boking</span>
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" href="/admin/history">
+          <i className="fas fa-fw fa-table" />
+          <span>History</span>
+        </Link>
+      </li>
+      <div className="text-center d-none d-md-inline">
+        <button className="rounded-circle border-0" id="sidebarToggle" />
+      </div>
+    </ul>
 
-    )
+  )
 }
